33d9f2fc4bcd0414912c4a030386f0e692fb8f5a
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-02-27 14:10:09 +0100

    Added community packs, changed colorscheme, added transparency

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.angular" },
 }
